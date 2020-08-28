824e468c712ac7f6baf415c7019a4bd746a3854d
taoz <16602720800@sina.cn>
2020-08-28 23:49:47 +0800

    我的代码提交

---
+++ src/pages/userTerminal/mine/answerDetail.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="answerDetail">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'answerDetail'
+  }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ src/pages/userTerminal/mine/setting.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="setting full-page">
+    <van-cell title="意见反馈" is-link />
+    <van-cell title="退出登录" is-link />
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'setting'
+  }
+</script>
+<style lang="scss">
+.van-cell:last-child::after {
+  display: inherit;
+}
+</style>
+<style lang="scss" scoped>
+  .setting{
+    padding-top: 30px;
+  }
+</style>
